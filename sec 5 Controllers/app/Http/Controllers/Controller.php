4e1122e6752d92f2eb30bdb6dbe1fabc2360aee1
SOHTA0318 <nicosohta0318@gmail.com>
2022-03-27 04:23:12 +0900

    basic controller

--- sec 5 Controllers/app/Http/Controllers/Controller.php
+++ sec 5 Controllers/app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+// namespace App\Http\functions1;
+// namespace App\Http\functions1;
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
--- sec 5 Controllers/routes/web.php
+++ sec 5 Controllers/routes/web.php
@@ -13,22 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/about', function () {
-    return "This is about page";
-});
-Route::get('/contact', function () {
-    return "This is contact page";
-});
-Route::get('/post/{id}/{name}',function($id,$name){
-    return "This is post number $id $name";
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/about', function () {
+//     return "This is about page";
+// });
+// Route::get('/contact', function () {
+//     return "This is contact page";
+// });
+// Route::get('/post/{id}/{name}',function($id,$name){
+//     return "This is post number $id $name";
+// });
 
-Route::get('/admin/posts/example',array('as'=>'admin.home',function(){
-    $url = route('admin.home');
-    return "This url is ". $url;
-}));
+// Route::get('/admin/posts/example',array('as'=>'admin.home',function(){
+//     $url = route('admin.home');
+//     return "This url is ". $url;
+// }));
+
+use App\Http\Controllers\PostsController;
+// Route::get('/post/{id}',[PostsController::class, 'index'])
+Route::resource('posts',PostsController::class);
 
 ?>
