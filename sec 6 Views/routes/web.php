ed0be63d60fbd0bdca060a0e0e156587ffbcfb0e
SOHTA0318 <nicosohta0318@gmail.com>
2022-03-28 11:38:33 +0900

    database crud

--- sec 6 Views/routes/web.php
+++ sec 6 Views/routes/web.php
@@ -35,8 +35,32 @@ use App\Http\Controllers\PostsController;
 // Route::get('/post/{id}',[PostsController::class, 'index'])
 // Route::resource('posts',PostsController::class);
 
-Route::get('/contact',[PostsController::class, 'contact'] );
+// Route::get('/contact',[PostsController::class, 'contact'] );
 
-Route::get('post/{id}/{name}/{password}',[PostsController::class,'show_post'])
+// Route::get('post/{id}/{name}/{password}',[PostsController::class,'show_post'])
 
+use Illuminate\Support\Facades\DB;
+// Route::get('/insert',function(){
+//   DB::insert('insert into posts(title, content) values (?, ?)',['PHP with Laravel', 'Laravel is the best thing that has happened to PHP']);
+// })
+
+// Route::get('/read',function(){
+//   $result = DB::select('select * from posts where id = ?',[1]);
+      // return $result;
+//   // foreach ($result as $post) {
+//   //   # code...
+//   //   return $post->title; 
+    
+//   // }
+// })
+
+// Route::get('/update',function(){
+//   $updated = DB::update('update posts set title = ? where id = ?',['Updated PHP',1]);
+//   return $updated;
+// })
+
+Route::get('/delete',function(){
+  $deleted = DB::delete('delete from posts where id = ?',[1]);
+  return $deleted;
+})
 ?>
