9bd8731f9c51cddf5f6e7ae14fc6e21dc49a0fe3
SOHTA0318 <nicosohta0318@gmail.com>
2022-04-03 14:26:14 +0900

    upload & store file

---
+++ sec 21 Form - Upload files/resources/views/posts/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('content')
+<h1>Create Post</h1>
+{{-- <form action="/posts" method="post"> --}}
+  {!! Form::open(['method'=>'POST','action'=>'App\Http\Controllers\PostsController@store','files'=>true]) !!}
+  @csrf
+
+  <div class="form-group">{!! Form::label('title', 'Title:') !!}
+  {!! Form::text('title', null, ['class'=>'form-control']) !!}
+  </div>
+
+  <div class="form-group">
+    {!! Form::file('file', ['class'=>'form-control']) !!}
+  </div>
+
+  <div class="form-group">
+{!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
+  </div>
+  {!! Form::close() !!}
+
+@if(count($errors) > 0)
+
+<div class="alert alert-danger">@foreach ($errors->all() as $error)
+  <li>{{$error}}</li>
+
+@endforeach</div>
+@endif
+
+@endsection
+
+@section('footer')
+
+@endsection
